2534ec599d41dc5fe39dbfc6411b9b317f67dc7f
carnoldcoding <carnoldcoding@gmail.com>
2022-03-21 13:38:36 -0400

    Size and reset working, now to change color

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const DEFAULT_SIZE = 16;
+let currentColor = "grey";
+let currentSize = DEFAULT_SIZE;
 
 //Make a mousedown flag so that coloring only happens when the mouse is clicked AND hovering
 let mouseDown = false;
@@ -9,7 +11,9 @@ document.body.onmouseup = () => (mouseDown = false)
 const button = document.querySelector(".button");
 button.addEventListener("click", resetGrid);
 
-let currentColor = "grey";
+const sizeButton = document.querySelector("#sizeButton");
+sizeButton.addEventListener("click", resizeGrid);
+
 
 function changeColor(e) {
     console.log(mouseDown);
@@ -20,6 +24,7 @@ function changeColor(e) {
 
 function setupGrid(size){
     const grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for(let i = 0; i < size * size; i++){
         const box = document.createElement("div");
         box.className = "box";
@@ -34,7 +39,20 @@ function resetGrid(){
     const boxes = document.querySelectorAll(".box");
     const grid = document.querySelector(".grid");
     boxes.forEach((box)=>{grid.removeChild(box);})
-    setupGrid(DEFAULT_SIZE);
+    setupGrid(currentSize);   
+}
+
+function resizeGrid(){
+    const size = prompt("Enter a grid size here: ", currentSize);
+    if(size < 100 && size > 0){
+        const boxes = document.querySelectorAll(".box");
+        const grid = document.querySelector(".grid");
+        boxes.forEach((box)=>{grid.removeChild(box);})
+        setupGrid(size);
+    }
+    else{
+        alert("Size is invalid");
+    }
 }
 
 window.onload = () =>{
